0ee79c2f4a7a606f46e13222a4fd6d90663700ad
Anestis Cheimonettos <chan20uf@student.ju.se>
2021-10-29 13:27:13 +0200

    first commit

---
+++ Lab01-TODO-LIST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab01-TODO-LIST
+//
+//  Created by Anestis Cheimonettos on 2021-10-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab01-TODO-LIST/Lab01_TODO_LISTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01_TODO_LISTApp.swift
+//  Lab01-TODO-LIST
+//
+//  Created by Anestis Cheimonettos on 2021-10-29.
+//
+
+import SwiftUI
+
+@main
+struct Lab01_TODO_LISTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
