e4cb4f68ace5c136c0944e51553eb42d8133f630
Anestis Cheimonettos <chan20uf@student.ju.se>
2021-11-02 17:53:36 +0300

    Added the functionality of deleting the selected record in Task List

--- Lab01-TODO-LIST/HistoryView.swift
+++ Lab01-TODO-LIST/HistoryView.swift
@@ -18,7 +18,18 @@ struct HistoryView: View {
 //            })
             ForEach( list, id: \.id  ) {  task in
                 if !task.isActive{
+                    HStack{
                     Text(task.taskDesc)
+                    Button("Delete task", action:{
+                        var found: Int = 0  // <= will hold the index if it was found, or else will be nil
+                        for i in (0...list.count-1) {
+                            if list[i].id == task.id {
+                                found = i
+                            }
+                        }
+                        list.remove(at: found)
+                    })
+                }
                 }
                             
             }
@@ -26,8 +37,8 @@ struct HistoryView: View {
     }
 }
 
-struct HistoryView_Previews: PreviewProvider {
-    static var previews: some View {
-        TodoView( list: .constant([]))
-    }
-}
+//struct HistoryView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TodoView( list: .constant([]))
+//    }
+//}
